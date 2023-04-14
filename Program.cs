c94c56163523262a8ca435f5afaaf52b8a6c3b52
BlindTerran <hongtingsu@gmail.com>
2023-04-15 00:01:10 +1000

    Level A update (break week)

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter a number:");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter a number:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter a number:");
+        int num3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(averageOfThree(num, num2, num3));
+    }
+
+    static int averageOfThree(int num1, int num2, int num3)
+    {
+        int[] numbers = new int[] { num1, num2, num3 };
+
+        int result = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            result += numbers[i];
+        }
+
+        return result / 3;
+    }
+}
